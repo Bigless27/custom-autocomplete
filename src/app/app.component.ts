48e46c143c7db2ecde4745cce22ea144d2fdb397
Bigless27 <lessonryan@gmail.com>
2017-10-24 19:12:25 -0700

    complete object property auto-complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,16 @@ export class AppComponent implements OnInit {
   stateForm: FormGroup;
 
   showDropDown = false;
-  states = ['Alabama', 'Alaska',  'Arizona', 'Arkansas', 'California', 'Colorado',
-  'Connecticut', 'Delaware', 'District of Columbia', 'Florida'
-  , 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky'
-  , 'Louisiana', 'Maine', 'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
-  'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina',
-  'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico',
-  'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington',
-   'West Virginia', 'Wisconsin', 'Wyoming'];
+  // states = ['Alabama', 'Alaska',  'Arizona', 'Arkansas', 'California', 'Colorado',
+  // 'Connecticut', 'Delaware', 'District of Columbia', 'Florida'
+  // , 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky'
+  // , 'Louisiana', 'Maine', 'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
+  // 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina',
+  // 'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico',
+  // 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington',
+  //  'West Virginia', 'Wisconsin', 'Wyoming'];
+
+  statesObj = [{name: 'Texas'}, {name: 'Michigan'}, {name: 'Washington'}]
 
   constructor( private fb: FormBuilder) {
     this.initForm()
--- src/app/shared/filter-pipe.ts
+++ src/app/shared/filter-pipe.ts
@@ -2,12 +2,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'searchFilter'})
 export class SearchFilterPipe implements PipeTransform {
-    transform(value: any, search: string): any {
+    transform(value: any, search: string, prop = null): any {
          if  (!search) {return value; }
-         let solution = value.filter(v => {
-            if ( !v ) {return;}
-           return  v.toLowerCase().indexOf(search.toLowerCase()) !== -1;
-        })
-        return solution;
+         if (prop) {
+            let solution = value.filter(v => {
+                if ( !v ) { return; }
+                return v[prop].toLowerCase().indexOf(search.toLowerCase()) !== -1;
+            });
+            return solution;
+         }else {
+             let solution = value.filter(v => {
+                if ( !v ) { return; }
+               return  v.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+            });
+            return solution;
+         }
     }
 }
