6d1195eb0bd145fa09ad4bf22e65fbc346a02562
Bigless27 <lessonryan@gmail.com>
2017-10-24 17:48:28 -0700

    add pipe properties and fix alignment

--- src/app/shared/letter-bold.pipe.ts
+++ src/app/shared/letter-bold.pipe.ts
@@ -4,26 +4,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class LetterBoldPipe implements PipeTransform {
     transform(value: any, search: any): any {
         if (!search) { return value; }
-        let searchLength = search.length;
        let  holder = value.split('');
-       let indexAdder = 0;
-       let indexs = this.locations(search.toLowerCase(), value.toLowerCase() )
-       indexs = indexs.map( (x) => {
-          let solution =  x + indexAdder;
-          indexAdder += 2;
-          return solution;
-       })
+       let indexs = this.locations(search, value )  // to lower case or use lowercase pipe
        indexs.forEach( (i) => {
             holder.splice(i, 0 , "<span class = 'bold'>")
-            holder.splice(i + searchLength +1, 0,  "</span>")
+            holder.splice(i + search.length + 1, 0,  "</span>")
        })
         return holder.join('');
     }
 
+//dont forget to lower case;
     locations(substring, searchValue) {
-        let indexs = [], i = -1;
-        while((i = searchValue.indexOf(substring, i + 1 )) >= 0) {
-             indexs.push(i);
+        let indexs = [], i = -1, spacer = 0;
+        while ( (i = searchValue.indexOf(substring, i + 1 )) >= 0) {
+            indexs.length > 0  ? indexs.push( i + (spacer += 2 ))  : indexs.push(i);
         }
         return indexs;
     }
